19e58fac95c5159f4520f3699ce54fffc093088d
Jovan Alleyne <jovan.alleyne@gmail.com>
2011-12-27 12:31:55 -0500

    - added helper functions - cleaned up old methods already declared in i/functions.inc.php

--- src/bin/php/functions.inc.php
+++ src/bin/php/functions.inc.php
@@ -22,12 +22,169 @@
 */
 
 
+/*
+ *
+ */
+function pprint_r($val){
+	echo "<pre>";
+	print_r($val);
+	echo "</pre>";
+	return TRUE;
+}
+
+/*
+ *
+ */
+function long_date($date){
+	return date("F j, Y",strtotime($date));
+}
 
 
+/*
+ *
+ */
+function hst($val){
+	return 0.13*(float)$val;
+}
 
-/**
-* Functions
-*/
+/*
+ *
+ */
+function parse_uri_parts($uri){
+	return array_merge(
+				array(),
+				array_filter(explode('/',$uri))
+			);
+}
+
+function get_uri_value($uri,$index){
+	$parts =  parse_uri_parts($uri);
+	return $parts[$index];
+}
+
+/*
+ *
+ */
+function process_form_post($uri,$content_type="application/x-www-form-urlencoded"){
+	if( isset($_POST) && count($_POST) ){
+		
+		/* */
+		$api = new HTTPRequest();
+		$api->base_uri = WS_URI;
+
+		/* */
+		$data = array_merge(array(),$_POST);
+
+		/* */
+		if( isset($data['response_redirect']) )
+			unset($data['response_redirect']);
+
+		/* */
+		return $api->post(
+						$uri,
+						$data,
+						'json',
+						$content_type
+					);
+	}
+	return NULL;
+}
+
+
+/*
+ *
+ */
+function route_as_inception_request($uri){	
+	/* Instanciate InceptionRESTfulDataApplication class */
+	$i = new InceptionRESTfulDataApplication($uri);
+
+	/* Load Route map from file system */
+	$i->loadRouteMap(ROUTE_FILE_PATH);
+
+	/* Parse loaded XML Route map */
+	if( $map=$i->parseRouteMap() ){
+
+		/* Read request data based on request method */
+		$i->readRequestData();
+
+		/* Determine if user can access resource based on Route definition */
+		if( $i->canAccess() ){
+
+			/* Validate request parameters */
+			if( $errors = $i->validateRequest() ){
+
+				/* */
+				$response = new HTTPRequestError(
+									'ResourceParameterException',
+									'Required parameters not included.'
+								);
+				$response->data = $errors;
+				return $response;
+			}
+			else{
+				/* Delegate request to mapped handler */
+				return $i->delegateRequest();
+			}
+		}
+		/* Respond with access denied */
+		else {
+			return new HTTPRequestError(
+								'AuthenticationException',
+								'Access to the requested resource has been denied.'
+							);
+		}
+	}
+
+	/* Route was found but no method mapped to $_SERVER['REQUEST_METHOD'] */
+	else if( $map === FALSE ){
+		/* */
+		return new HTTPRequestError(
+							'ResourceHandlerException',
+							'The request [:'.$i->uri.'] does not support '.
+							'the request method [:'.$_SERVER['REQUEST_METHOD'].'].'
+						);
+
+	}
+
+	/* No Route found in mapping data */
+	else {
+		/* */
+		return new HTTPRequestError(
+							'ResourceHandlerException',
+							'The request [:'.$i->uri.'] does not exist.'
+						);
+	}
+}
+
+/*
+ *
+ */
+function as_json($response){
+	/* */
+	return json_decode(
+			(string)($response),TRUE);
+}
+
+
+/*
+ *
+ */
+function get_user(){
+	/* */
+	$api = new HTTPRequest();
+	$api->base_uri = WS_URI;
+
+	/* */
+	$r = $api->get(
+					'/me/'
+				);
+	
+	/* */
+	if( isset($r['data']) )
+		return $r['data'];
+	else return NULL;
+}
+
 
 
 /*
@@ -65,219 +222,192 @@ function print_queued_inline_js(){
 }
 
 
-
-
-/* */
-define('CRLF',"\n");
-
-/**
-*
-*/
-function is_valid_email($str){
-	$atIndex = strrpos($str, "@");
-	if (is_bool($atIndex) && !$atIndex) {
-			return false;
-		} else {
-			$domain = substr($str, $atIndex +1);
-			$local = substr($str, 0, $atIndex);
-			$localLen = strlen($local);
-			$domainLen = strlen($domain);
-			if ($localLen < 1 || $localLen > 64) {
-				// local part length exceeded
-				return false;
-			} else
-				if ($domainLen < 1 || $domainLen > 255) {
-					// domain part length exceeded
-					return false;
-				} else
-					if ($local[0] == '.' || $local[$localLen -1] == '.') {
-						// local part starts or ends with '.'
-						return false;
-					} else
-						if (preg_match('/\\.\\./', $local)) {
-							// local part has two consecutive dots
-							return false;
-						} else
-							if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
-								// character not valid in domain part
-								return false;
-							} else
-								if (preg_match('/\\.\\./', $domain)) {
-									// domain part has two consecutive dots
-									return false;
-								} else
-									if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\", "", $local))) {
-										if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\", "", $local))) {
-											return false;
-										}
-									}
-			if (!(checkdnsrr($domain, "MX") || checkdnsrr($domain, "A"))) {
-				// domain not found in DNS
-				return false;
-			}
-	}
-	return true;
-}
-
-/**
-*
-*/
-function is_valid_phone( $phone ){
-	return true;
-}
-
-/**
-*
-*/
-function is_valid_password( $pwd ){
-	return strlen($pwd)>4;
-}
-
-/**
-*
-*/
-function is_valid_postalcode( $pcode ){
-	$pc_regex = '/^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$/i';
-	return preg_match($pc_regex, $pcode);
-}
-
-
-
-/**
-*
-*/
-function mysql_resultset_to_array($r){
-	$col = array();
-	while( $item = mysql_fetch_assoc($r) )
-		$col[] = $item;
-		
-	return $col;
-}
-
-/**
-*
-*/
-function get_message_template( $message_path ){
-	return file_get_contents( $message_path );
-}
-
-/**
-*
-*/
-function replace_message_vars($vars,$values,$message){
-	return str_replace(
-							$vars,
-							$values,
-							$message
-						);
-}
-
-
-/**
-*
-*/
-function email_headers($from,$from_name){
-		$headers  = 'MIME-Version: 1.0'.CRLF;
-		$headers .= 'From: "'.$from_name.'" <'.$from.'>'.CRLF;
-		$headers .= 'Reply-To: '.$from.CRLF;
-		$headers .= 'Return-Path: <'.$from.'>'.CRLF;
-		$headers .= 'Content-Type: text/plain'. CRLF;
-		$headers .= 'Content-Transfer-Encoding: utf-8' . CRLF;
-		$headers .= 'X-Mailer: PHP/' . phpversion() . CRLF;
-		
-		return $headers;
-}
-
-
-/**
-*
-*/
-
-function send_email( $subject, $recepientName, $to, $from, $from_name, $message ){
-		/*
-		 * send messages to recepient
-		 */
-		 
-		return mail( 
-							"$recepientName<$to>", 
-							$subject, 
-							$message,
-							email_headers( $from, $from_name ) 
-						);
-}
-
-
-
-
-/**
- * Indents a flat JSON string to make it more human-readable.
+/*
  *
- * @param string $json The original JSON string to process.
- *
- * @return string Indented version of the original JSON string.
  */
-function json_pretty_print($json) {
 
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
 
-    for ($i=0; $i<=$strLen; $i++) {
+/**
+ *
+ */
+function write_html_select($data,$label,$value=NULL,$select=NULL){
+	foreach( $data as $opt ){
+		$opt_val	= $opt[$value?$value:$label];
+		$opt_label 	= $opt[$label];
+		$selected	= $select==$opt_val?' selected="selected"':'';
+		echo '<option value="'.$opt_val.'"'.$selected.'>'.$opt_label.'</option>';
+	}
+}
 
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
 
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-        
-        // If this character is the end of an element, 
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        // Add the character to the result string.
-        $result .= $char;
 
-        // If the last character was the beginning of an element, 
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-            
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        $prevChar = $char;
-    }
 
-    return $result;
+
+/**
+ *
+ */
+function build_multipart_body($data,$boundary=NULL){
+	/* */
+	$boundary = !$boundary?generate_multipart_boundary():$boundary;
+	$boundary = '--'.$boundary;
+	/* */
+	$request_body = $boundary.PHP_EOL;
+	/* */
+	$request_values = array();
+	foreach( $data as $k=>$v )
+		$values = build_multipart_value($request_values,$k,$v,$boundary);
+		
+	/* */
+	foreach( $_FILES as $k=>$file ){
+		/* */
+		if( is_array($file['error']) )
+			$k .= '[]';
+		/* */
+		/* */
+		if( is_array($file['error']) ) $filename = $file['name'][0];
+		else $filename = $file['name'];
+		
+		$request_values[] = "Content-Disposition: form-data; name=\"$k\"; filename=\"$filename\" ";
+			
+		/* */
+		$request_values[] = "Content-Type: application/octet-stream".PHP_EOL;
+		/* */
+		if( is_array($file['error']) ) $tmp_name = $file['tmp_name'][0];
+		else $tmp_name = $file['tmp_name'];
+			
+		$request_values[] = @file_get_contents($tmp_name);
+		
+		/* */	
+		$request_values[] = $boundary;
+	}
+		
+	/* */
+	$request_body .= implode(PHP_EOL,$request_values);
+	/* */
+	$request_body .= '--'.PHP_EOL;
+	
+	/* */
+	return "Content-Length: ".strlen($request_body).PHP_EOL.$request_body;
+}
+
+
+function build_multipart_value(&$request,$key,$value,$boundary,$nest=0){
+	if( is_array($value) ){
+		/* */
+		foreach( $value as $k=>$v)
+			build_multipart_value($request,$key,$v,$boundary,$nest+1);
+	}
+	else {
+		/* */
+		for($i=0;$i<$nest;$i++)
+			$key .= '[]';	
+		/* */
+		$request[] = "Content-Disposition: form-data; name=\"$key\"".PHP_EOL;
+		$request[] = $value;
+		$request[] = $boundary;
+	}
 }
 
 /**
-*
-*/
-function validate($val,$type){
-	
-	switch($type){
-		case 'email':
-			if( !is_valid_email( $val ) ) return "Invalid email address"; 
-			break;
-	}
-	
-	return null;
+ *
+ */
+function generate_multipart_boundary(){
+	return '---------------------------'.base64_encode(md5(microtime().rand(0,getrandmax())));
 }
 
+
+
+
+/**
+ *
+ */
+function write_ini_file($assoc_arr, $path, $has_sections=TRUE) { 
+	
+	
+    $content = ""; 
+    if ($has_sections) { 
+        foreach ($assoc_arr as $key=>$elem) { 
+            $content .= "[".$key."]\n"; 
+            foreach ($elem as $key2=>$elem2) { 
+                if(is_array($elem2)) 
+                { 
+                    for($i=0;$i<count($elem2);$i++) 
+                    { 
+                        $content .= $key2."[] = \"".$elem2[$i]."\"\n"; 
+                    } 
+                } 
+                else if($elem2=="") $content .= $key2." = \n"; 
+                else $content .= $key2." = \"".$elem2."\"\n"; 
+            } 
+        } 
+    } 
+    else { 
+        foreach ($assoc_arr as $key=>$elem) { 
+            if(is_array($elem)) 
+            { 
+                for($i=0;$i<count($elem);$i++) 
+                { 
+                    $content .= $key2."[] = \"".$elem[$i]."\"\n"; 
+                } 
+            } 
+            else if($elem=="") $content .= $key2." = \n"; 
+            else $content .= $key2." = \"".$elem."\"\n"; 
+        } 
+    } 
+
+    if (!$handle = fopen($path, 'w')) { 
+        return false; 
+    } 
+    if (!fwrite($handle, $content)) { 
+        return false; 
+    } 
+    fclose($handle); 
+    return true; 
+}
+
+
+
+function generate_ini_settings_fields($config){
+	/* */
+	$fields = array();
+	foreach( $config as $section=>$settings ){
+		
+		/* */
+		$section_label = str_replace('_',' ',$section);
+		
+		/* */
+		$fields[] = "<fieldset>";
+		$fields[] = "<legend>$section_label</legend>";
+		/* */
+		$fields[] = "<ol>";
+		/* */
+		foreach( $settings as $key=>$value )
+			write_ini_settings_input($key,$value,$fields,$section);
+
+		/* */
+		$fields[] = "</ol>";
+		$fields[] = "</fieldset>";
+	}
+	/* */
+	return implode(PHP_EOL,$fields);
+}
+
+
+function write_ini_settings_input($name,$value,&$form=array(),$section=NULL){
+	/* */
+	if( $section ){
+		$field_name = $section."[$name]";
+	}
+	/* */
+	$label 	= str_replace('_',' ',$name);
+	$id 	= 'settings_'.str_replace(' ','_',$label);
+	/* */
+	$form[] = "	<li>";
+	$form[] = "<label title=\"This variable is displayed in the title of the browser among other places on the site\" for=\"$id\">$name</label>";
+	
+	$form[] = "<textarea name=\"$field_name\" id=\"$id\">$value</textarea>";
+	//$form[] = "<input id=\"siteName\" class=\"required\" type=\"text\" size=\"60\" value=\"$value\" name=\"siteName\">";
+	$form[] = "	</li>";
+	
+	return $form;
+}
--- src/bin/settings.php
+++ src/bin/settings.php
@@ -4,26 +4,69 @@
  * 
  */
 
+$permissions = array(
+	'read' 		=> 1,
+	'write'		=> 2,
+	'update'	=> 4,
+	'delete'	=> 8
+);
+
+
+
 
 /* Google analytics account number - format UA-XXXXX-Y */
 define('ANALYTICS_UA','UA-XXXXX-Y');
 
 
 /* Define Database Access constants */
-if( $_SERVER['HTTP_HOST']!='localhost' ){
-	
-	define('DB_HOST','localhost');
-	define('DB_USER','username');
-	define('DB_PASS','password');
-	define('DB_NAME','database_name');
-	
-}else{
-	
-	/* Local server */
-	define('DB_HOST','localhost');
-	define('DB_USER','username');
-	define('DB_PASS','password');
-	define('DB_NAME','database_name');
+switch( $_SERVER['HTTP_HOST'] ){
+	/* Local Development server*/
+	case 'localhost':
+		define('DB_HOST','localhost');
+		define('DB_USER','dbuser_ecot');
+		define('DB_PASS','5u@@u3Re7eDu');
+		define('DB_NAME','ecot');
+		break;
+	/* Live server*/
+	case 'eco.dev.transmitterstudios.com':
+		define('DB_HOST','localhost');
+		define('DB_USER','economic_dev');
+		define('DB_PASS','passw0rd');
+		define('DB_NAME','economic_dev2');
+		break;	
+	/* Live server*/
+	default:
+		define('DB_HOST','localhost');
+		define('DB_USER','economic_live');
+		define('DB_PASS','9LGUZWyczDvP');
+		define('DB_NAME','economic_cms2');
+		break;
+}
+
+
+/* */
+$acceptable_mime_types = array(
+							'image/pjpeg',
+							'image/jpeg',
+							'image/x-png',
+							'image/png',
+							
+							'application/pdf',
+							
+							'video/quicktime',
+							
+							'audio/mp3'
+						);
+
+/* */
+$upload_folder = "/media/";
+
+if( !is_writable($_SERVER['DOCUMENT_ROOT'].$upload_folder) ){
+	chmod($_SERVER['DOCUMENT_ROOT'].$upload_folder, 0777);
+	if( !is_writable($_SERVER['DOCUMENT_ROOT'].$upload_folder) )
+		trigger_error(
+				'Uploads folder is not writable and the script '.
+				'was unable to modify the directory permissions.');
 }
 
 
@@ -46,6 +89,7 @@ define('BASE_URI',		(isset($_SERVER['HTTPS'])&&
 						);
 
 
+
 /* 
 * Set content root based on language setting, if 
 * folder does not exist look for default language folder 
@@ -55,3 +99,11 @@ define('CONTENT_ROOT',
 			(is_dir($_SERVER['DOCUMENT_ROOT'].'/'.LANGUAGE)?
 			LANGUAGE:getenv('DEFAULT_LANGUAGE'))
 	);
+	
+	
+	
+
+/* */
+$path  = implode(PATH_SEPARATOR,array(CONTENT_ROOT));
+set_include_path( get_include_path() . PATH_SEPARATOR . $path );
+
