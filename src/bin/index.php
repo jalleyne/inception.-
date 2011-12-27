3fd0c233f24ad31668c090c196396bebe36a9602
Jovan Alleyne <jovan.alleyne@gmail.com>
2011-12-27 12:29:30 -0500

    - refactored for better include structure

--- src/bin/index.php
+++ src/bin/index.php
@@ -25,21 +25,43 @@
 
 
 
-/* 
-* Set language constant, first check the cookie 
-* then resort to default language set in .htaccess file
-*/
-define('LANGUAGE', 		
-		empty($_COOKIE['LANGUAGE'])?
-		getenv('DEFAULT_LANGUAGE'):$_COOKIE['LANGUAGE']);
-
-
 /**
- * NOTE: This is not your content file. Place all content inside your localized
+ * NOTE: This is not your the place to put your html. Place all content inside your localized
  * folder. example /en-CA/index.php will replace what your would expect here as
  * your default page.
  */
 
 
+
+/* Begin session */
+if (!session_id()) {
+	session_start();
+}
+
+/** 
+ * Set language constant, first check the cookie 
+ * then resort to default language set in .htaccess file
+ */
+if( !defined('LANGUAGE') )
+	define('LANGUAGE', 		
+				empty($_COOKIE['LANGUAGE'])?
+				getenv('DEFAULT_LANGUAGE'):$_COOKIE['LANGUAGE']
+			);
+
 /* */
+require_once $_SERVER['DOCUMENT_ROOT'].'/i/settings.php';
+
+
+
+/*
+ * 
+ */
+$api = new HTTPRequest();
+$api->base_uri = WS_URI;
+
+
+/**
+ * Include page proxy file 
+ * (handles content negotiation) 
+ */
 require_once "pageproxy.php";
