ea2ac9737615ec233e31084c56a32575a3c7535d
Jovan Alleyne <jovan.alleyne@gmail.com>
2011-12-27 13:34:52 -0500

    - changed default settings

--- src/bin/settings.php
+++ src/bin/settings.php
@@ -4,12 +4,6 @@
  * 
  */
 
-$permissions = array(
-	'read' 		=> 1,
-	'write'		=> 2,
-	'update'	=> 4,
-	'delete'	=> 8
-);
 
 
 
@@ -23,50 +17,18 @@ switch( $_SERVER['HTTP_HOST'] ){
 	/* Local Development server*/
 	case 'localhost':
 		define('DB_HOST','localhost');
-		define('DB_USER','dbuser_ecot');
-		define('DB_PASS','5u@@u3Re7eDu');
-		define('DB_NAME','ecot');
+		define('DB_USER','dev_user');
+		define('DB_PASS','password');
+		define('DB_NAME','database_name');
 		break;
-	/* Live server*/
-	case 'eco.dev.transmitterstudios.com':
-		define('DB_HOST','localhost');
-		define('DB_USER','economic_dev');
-		define('DB_PASS','passw0rd');
-		define('DB_NAME','economic_dev2');
-		break;	
+	
 	/* Live server*/
 	default:
 		define('DB_HOST','localhost');
-		define('DB_USER','economic_live');
-		define('DB_PASS','9LGUZWyczDvP');
-		define('DB_NAME','economic_cms2');
+		define('DB_USER','dev_user');
+		define('DB_PASS','password');
+		define('DB_NAME','database_name');
 		break;
-}
-
-
-/* */
-$acceptable_mime_types = array(
-							'image/pjpeg',
-							'image/jpeg',
-							'image/x-png',
-							'image/png',
-							
-							'application/pdf',
-							
-							'video/quicktime',
-							
-							'audio/mp3'
-						);
-
-/* */
-$upload_folder = "/media/";
-
-if( !is_writable($_SERVER['DOCUMENT_ROOT'].$upload_folder) ){
-	chmod($_SERVER['DOCUMENT_ROOT'].$upload_folder, 0777);
-	if( !is_writable($_SERVER['DOCUMENT_ROOT'].$upload_folder) )
-		trigger_error(
-				'Uploads folder is not writable and the script '.
-				'was unable to modify the directory permissions.');
 }
 
 
